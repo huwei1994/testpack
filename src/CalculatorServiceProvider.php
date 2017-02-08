1486b0e112f7911eb7fe69fb27bc829768777b1f
huwei <wei.hu@haoqitech.com.cn>
2017-02-08 17:06:28 +0800

    再试试

--- src/CalculatorServiceProvider.php
+++ src/CalculatorServiceProvider.php
@@ -14,7 +14,7 @@ class CalculatorServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        include __DIR__.'/routes.php';
+        include __DIR__ . '/routes.php';
     }
 
     /**
--- src/routes.php
+++ src/routes.php
@@ -1,3 +1,4 @@
 <?php
-Route::get('add/{a}/{b}', 'Devdojo\Calculator\CalculatorController@add');
-Route::get('subtract/{a}/{b}', 'Devdojo\Calculator\CalculatorController@subtract');
+Route::get('calculator', function(){
+    echo 'Hello from the calculator package!';
+});
